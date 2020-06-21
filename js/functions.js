d6196eba409a7ff64aac75696104ccf3d0bf344f
EmiSan <s.faraldo122@gmail.com>
2020-06-21 14:55:15 -0300

    javascript chill

--- js/functions.js
+++ js/functions.js
@@ -1,139 +1,126 @@
-function canvas(){ 
-    const canvas = document.getElementById('hangman');
-    const context = canvas.getContext("2d");
+// ### VARIABLES ###
 
-    clearCanvas = () => {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    }
-
-    Horca = () => { 
-    context.strokeStyle = 'whitesmoke';
-    context.lineWidth = 10; 
-    context.beginPath();
-    //base
-    context.moveTo(175, 225);
-    context.lineTo(5, 225);
-    //palo principal
-    context.moveTo(40, 225);
-    context.lineTo(25, 5);
-    context.lineTo(100, 5);
-    context.lineTo(100, 25);
-    //palito diagonal
-    context.moveTo(30, 55);
-    context.lineTo(50, 5);
-    context.stroke();}
-
-    Horca();
-    Draw = (part) => {
-    switch (part) {
-        case 'head':
-            context.lineWidth = 5;
-            context.beginPath();
-            context.arc(100, 50, 25, 0, Math.PI*2, true);
-            context.closePath();
-            context.stroke();
-            break;
-        
-        case 'body':
-            context.beginPath();
-            context.moveTo(100, 75);
-            context.lineTo(100, 140);
-            context.stroke();
-            break;
-
-        case 'rightHarm':
-            context.beginPath();
-            context.moveTo(100, 85);
-            context.lineTo(60, 100);
-            context.stroke();
-            break;
-
-        case 'leftHarm':
-            context.beginPath();
-            context.moveTo(100, 85);
-            context.lineTo(140, 100);
-            context.stroke();
-            break;
-
-        case 'rightLeg':
-            context.beginPath();
-            context.moveTo(100, 140);
-            context.lineTo(80, 190);
-            context.stroke();
-            break;
-
-        case 'rightFoot':
-            context.beginPath();
-            context.moveTo(82, 190);
-            context.lineTo(70, 185);
-            context.stroke();
-        break;
-
-        case 'leftLeg':
-            context.beginPath();
-            context.moveTo(100, 140);
-            context.lineTo(125, 190);
-            context.stroke();
-        break;
-
-        case 'leftFoot':
-            context.beginPath();
-            context.moveTo(122, 190);
-            context.lineTo(135, 185);
-            context.stroke();
-        break;
-    } 
-    }
-
-    const draws = [ 
-    'head', 
-    'body', 
-    'rightHarm', 
-    'leftHarm',
-    'rightLeg',
-    'leftLeg',
-    'rightFoot',
-    'leftFoot',
-    ]
-    var step = 0;
+// Array de palabras
+var palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
+// Palabra a averiguar
+var palabra = "";
+// Nº aleatorio
+var rand;
+// Palabra oculta
+var oculta = [];
+// Elemento html de la palabra
+var hueco = document.getElementById("palabra");
+// Contador de intentos
+var cont = 6;
+// Contador atrasado
+var contA = 6;
+// Botones de letras
+var buttons = document.getElementsByClassName('letra');
+// Boton de reset
+var btnInicio = document.getElementById("reset");
+// Partidas Ganadas
+window.ganadas=0;
+// Partidas Perdidas
+window.perdidas=0;
 
 
-    const next = document.getElementById('next')
+// ### FUNCIONES ###
 
-    next.addEventListener('click', function() {
-    Draw(draws[step++])
-    if (undefined === draws[step]) this.disabled = true;
-    });
-
-    document.getElementById('reset').addEventListener('click', function() {
-    clearCanvas()
-    Horca()
-    step = 0
-    next.disabled = false
-    })
+// Escoger palabra al azar
+function generaPalabra() {
+  rand = (Math.random() * 19).toFixed(0);
+  palabra = palabras[rand][0].toUpperCase();
+  console.log(palabra);
 }
 
-var comidas  = ['Milanesa', 'Pure', 'Pollo', 'Hamburguesa', 'Papas Fritas', 'Pizza', 'Chivito'];
-var pistacomida = ['Carne enpanizada', 'Suave y Blanco', '', 'Carne redonda al pan', 'Bastones fritos', 'Masa redonda con salsa', 'Carne al pan'];
-var peliculas  = ['Deadpool', 'Spiderman', 'Transformers', 'Starwars', 'Avengers', 'Los juegos del Hambre', 'Harry Potter'];
-var pistapeliculas = ['', '', '', '', '', '', ''];
-var adivinar ="";
+// Funcion para pintar los guiones de la palabra
+function pintarGuiones(num) {
+  for (var i = 0; i < num; i++) {
+    oculta[i] = "_";
+  }
+  hueco.innerHTML = oculta.join("");
+}
 
-function generaPalabraComida() {
-    var rand = (Math.random() * 7).toFixed(0);
-    adivinar = comida[rand].toUpperCase();
-    console.log(adivinar);
+//Generar abecedario
+function generaABC (a,z) {
+  document.getElementById("abecedario").innerHTML = "";
+  var i = a.charCodeAt(0), j = z.charCodeAt(0);
+  var letra = "";
+  for( ; i<=j; i++) {
+    letra = String.fromCharCode(i).toUpperCase();
+    document.getElementById("abecedario").innerHTML += "<button value='" + letra + "' onclick='intento(\"" + letra + "\")' class='letra' id='"+letra+"'>" + letra + "</button>";
+    if(i==110) {
+      document.getElementById("abecedario").innerHTML += "<button value='Ñ' onclick='intento(\"Ñ\")' class='letra' id='"+letra+"'>Ñ</button>";
+    }
   }
-  function pistaComida() {
-    document.getElementById("Pista").innerHTML = pistacomida[rand];
-  }
-  
-  function generaPalabraPelicula() {
-    var rand = (Math.random() * 7).toFixed(0);
-    adivinar = peliculas[rand][0].toUpperCase();
-    console.log(adivinar);
-  }
-  function pistaPeliculas() {
-    document.getElementById("Pista").innerHTML = pistapeliculas[rand];
-  }
+}
 
+// Chequear intento
+function intento(letra) {
+  document.getElementById(letra).disabled = true;
+  if(palabra.indexOf(letra) != -1) {
+    for(var i=0; i<palabra.length; i++) {
+      if(palabra[i]==letra) oculta[i] = letra;
+    }
+    hueco.innerHTML = oculta.join("");
+    document.getElementById("acierto").innerHTML = "Bien!";
+    document.getElementById("acierto").className += "acierto verde";
+  }else{
+    cont--;
+    document.getElementById("intentos").innerHTML = cont;
+    document.getElementById("acierto").innerHTML = "Fallo!";
+    document.getElementById("acierto").className += "acierto rojo";
+    document.getElementById("image"+contA).className = "oculto";
+    document.getElementById("image"+cont).className = "fade-in";
+    contA--;
+  }
+  compruebaFin();
+  setTimeout(function () { 
+    document.getElementById("acierto").className = ""; 
+  }, 800);
+}
+
+// Obtener pista
+function pista() {
+  document.getElementById("hueco-pista").innerHTML = palabras[rand][1];
+}
+
+// Compruba si ha finalizado
+function compruebaFin() {
+  if( oculta.indexOf("_") == -1 ) {
+    document.getElementById("acierto").innerHTML = "Felicidades !!";
+    document.getElementById("acierto").className += "zoom-in";
+    document.getElementById("palabra").className += " encuadre";
+    ganadas++;
+    document.getElementById("ganadas").innerHTML = ganadas
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").disabled = false;
+    document.getElementById("pista").disabled = true;
+    btnInicio.onclick = function() { location.reload() };
+  }else if( cont == 0 ) {
+    document.getElementById("acierto").innerHTML = "Game Over";
+    document.getElementById("acierto").className += "zoom-in";
+    perdidas++;
+    document.getElementById("perdidas").innerHTML = perdidas
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").disabled = false;
+    document.getElementById("pista").disabled = true;
+    btnInicio.onclick = function () { location.reload() };
+  }
+}
+
+// Restablecer juego
+function inicio() {
+  generaPalabra();
+  document.getElementById("reset").disabled = true;
+  pintarGuiones(palabra.length);
+  generaABC("a","z");
+  cont = 6;
+}
+
+// Iniciar
+window.onload = inicio();
